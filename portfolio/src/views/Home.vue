126ad0b9fca83180f3282986f47bd4bb2a437228
vigneshwaran-tech <83588204+vigneshwaran-tech@users.noreply.github.com>
2022-01-26 12:52:15 +0530

    changes

--- portfolio/src/views/Home.vue
+++ portfolio/src/views/Home.vue
@@ -1,60 +1,42 @@
 <template>
-<v-carousel
-        cycle
-        height="580"
-        hide-delimiter-background
-        show-arrows-on-hover
-      >
-        <v-carousel-item
-          v-for="(image, i) in images"
-          :key="i"
-          :src="image.src"
-        >
-        </v-carousel-item>
-      </v-carousel>
-    <!-- <carosoul/> -->
- 
+  <v-carousel cycle height="580" hide-delimiter-background show-arrows-on-hover>
+    <v-carousel-item v-for="(image, i) in images" :key="i" :src="image.src">
+      <v-row align="center" justify="center">
+      <v-col class="text-center mt-16" cols="12">
+        <h1 class="text-h2 white--text ">COMING SOON</h1>
+      </v-col>
+    </v-row>
+    </v-carousel-item>
+  </v-carousel>
 </template>
 
 <script>
-// import carosoul from './carosoul.vue'
 export default {
-  name: 'Home',
-  components:{
-    // carosoul,
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      images: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          // src:'1.jpg',
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+          // src:'2.jpg',
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+          // src:'logo.svg',
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+          // src:'4.jpg',
+        },
+      ],
+    };
   },
-   data () {
-      return {
-        colors: [
-          'indigo',
-          'warning',
-          'pink darken-2',
-          'red lighten-1',
-          'deep-purple accent-4',
-        ],
-        images: [
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-            // src:'1.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-            // src:'2.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-            // src:'logo.svg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-            // src:'4.jpg',
-          },
-        ],
-      }
-    },
-  
-}
+};
 </script>
 <style  lang="scss" scoped>
-
 </style>
